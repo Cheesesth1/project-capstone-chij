6d8150d1c326a88f709ad78fc4eca98a1081090a
CHIJ KUMAR SHRESTHA <cheese_sth@yahoo.com>
2023-12-04 11:41:19 -0600

    login page created

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,12 +3,13 @@ const Cart = () => {
     <div>
       <header>
         <div className="cart_icon">🛒</div>
+        <div className="wishlist_icon"></div>
       </header>
 
+      <button onClick={() => "addToCart(1)"}>Cart</button>
+      <button onClick={() => "addToWishlist(1)"}>Wish</button>
       <div className="product">
         <h2>Mountain Backpack</h2>
-        <button onClick={() => "addToCart(1)"}>Cart</button>
-        <button onClick={() => "addToWishlist(1)"}>Wish</button>
       </div>
     </div>
   );
